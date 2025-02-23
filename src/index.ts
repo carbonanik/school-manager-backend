b9f7ec6968a82d74480be41f92d2444d89f9974e
Anik <sheikhanikbd@gmail.com>
2025-02-23 12:48:50 +0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const port = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 app.use(express.json());
 
