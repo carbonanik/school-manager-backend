2a5816eea4563adfe733ed58eaa5b00c8869bdd5
Anik <sheikhanikbd@gmail.com>
2025-02-23 21:33:16 +0600

    fix

--- src/util/sessionMiddleware.ts
+++ src/util/sessionMiddleware.ts
@@ -1,3 +1,22 @@
+// import connectMemcached from "connect-memcached";
+// import session from "express-session";
+
+// const MemcachedStore = connectMemcached(session);
+// export var sessionMiddleware = session({
+//     store: new MemcachedStore({ hosts: [process.env.MEMCACHED_HOST || '127.0.0.1:11211'] }), // Connect to Memcached
+//     secret: process.env.SESSION_SECRET || 'supersecretkey',
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { 
+//         secure: false, 
+//         httpOnly: true, 
+//         maxAge: 1000 * 60 * 60 * 24, 
+//         sameSite: 'none',
+//         // domain: '.at-tahfiz-international-madrasha.com'
+//     },
+//     // proxy: true,
+// });
+
 import connectMemcached from "connect-memcached";
 import session from "express-session";
 
@@ -7,12 +26,5 @@ export var sessionMiddleware = session({
     secret: process.env.SESSION_SECRET || 'supersecretkey',
     resave: false,
     saveUninitialized: false,
-    cookie: { 
-        secure: false, 
-        httpOnly: true, 
-        maxAge: 1000 * 60 * 60 * 24, 
-        sameSite: 'none',
-        // domain: '.at-tahfiz-international-madrasha.com'
-    },
-    // proxy: true,
+    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 },
 });
